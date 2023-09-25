bb6fc7e04a367fd539fe3196a38f908c55b6bc87
rorosana <r.a.caraballo8@gmail.com>
2023-09-25 03:38:25 +0200

    almost done

--- app/Http/Controllers/SWAPIController.php
+++ app/Http/Controllers/SWAPIController.php
@@ -11,14 +11,17 @@ use Illuminate\Database\QueryException;
 
 class SWAPIController extends Controller
 {
+
+/**
+ * Obtiene datos de personajes humanos de la API de Star Wars y los almacena en caché.
+ *
+ * @return \Illuminate\Http\JsonResponse
+ */
    public function fetchAndStoreCharacters()
 {
-    // Comprueba si los datos ya están en caché
     if (cache()->has('swapi_characters')) {
-        // Si están en caché, obtén los datos desde la caché
         $characters = cache('swapi_characters');
     } else {
-        // Si no están en caché, realiza la solicitud a la API y almacena los datos en caché
         $humanCharacterUrls = [
             "https://swapi.dev/api/people/66/",
             "https://swapi.dev/api/people/67/",
@@ -33,26 +36,26 @@ class SWAPIController extends Controller
             $characterData = $characterResponse->json();
 
             if ($characterData['species'][0] === 'https://swapi.dev/api/species/1/') {
-                // Procesa y almacena los datos en el resultado
                 $result[] = $characterData;
             }
         }
 
-        // Almacena los datos en caché durante 24 horas
         cache()->put('swapi_characters', $result, now()->addHours(24));
 
-        // Establece la variable $characters con los datos recién obtenidos
         $characters = $result;
     }
 
     return response()->json(['message' => 'Datos de personajes humanos', 'characters' => $characters]);
 }
 
-
+/**
+ * Actualiza la caché de datos de personajes humanos obtenidos de la API de Star Wars.
+ *
+ * @return \Illuminate\Http\RedirectResponse
+ */
 public function refreshCache()
 {
     try {
-        // Realiza solicitudes a swapi.com para obtener los datos más recientes
         $humanCharacterUrls = [
             "https://swapi.dev/api/people/66/",
             "https://swapi.dev/api/people/67/",
@@ -66,32 +69,42 @@ public function refreshCache()
             $characterResponse = Http::get($characterUrl);
             $characterData = $characterResponse->json();
 
-            // Agrega los datos actualizados al arreglo
             $updatedData[] = $characterData;
         }
 
-        // Actualiza la caché con los datos actualizados
+
         cache()->put('swapi_characters', $updatedData, now()->addHours(24));
 
         return redirect()->route('home')->with('success', 'Caché actualizada con éxito');
     } catch (\Exception $e) {
-        // Maneja cualquier error que pueda ocurrir durante las solicitudes
         return redirect()->route('home')->with('error', 'Error al actualizar la caché: ' . $e->getMessage());
     }
 }
 
 
-
+/**
+ * Muestra la página de búsqueda de personajes con filtros. Si no las variables aparecían como undefined, no se cargaba la vista.
+ *
+ * @return \Illuminate\View\View
+ */
 
 public function index()
 {
     $hairColors = HairColor::distinct()->pluck('nombre');
     $skinColors = SkinColor::distinct()->pluck('nombre');
-    $characters = []; // Inicializa la variable de personajes vacía
+    $characters = [];
+
+    //dd($characters);
 
 
     return view('characters_finder', compact('characters', 'hairColors', 'skinColors'));
 }
+
+/**
+ * Obtiene una lista de colores de cabello y piel y devuelve una respuesta JSON. Para rellenar los options de los selects.
+ *
+ * @return \Illuminate\Http\JsonResponse
+ */
 
 
 public function getColors()
@@ -102,54 +115,70 @@ public function getColors()
     return response()->json(['hairColors' => $hairColors, 'skinColors' => $skinColors]);
 }
 
-
+/**
+ * Realiza una búsqueda de personajes según el término de búsqueda y devuelve una vista con los resultados.
+ *
+ * @param  \Illuminate\Http\Request  $request
+ * @return \Illuminate\Contracts\View\View
+ */
 
 public function search(Request $request)
 {
     try {
-        $searchTerm = $request->input('search_term'); // Obtiene el término de búsqueda del usuario desde el formulario
+        $searchTerm = $request->input('search_term');
 
-        // Realiza la consulta para obtener los personajes que coinciden con el color de piel o cabello
-        $characters = Characters::whereHas('skinColor', function ($query) use ($searchTerm) {
-            $query->where('nombre', 'like', '%' . $searchTerm . '%');
-        })
-        ->orWhereHas('hairColor', function ($query) use ($searchTerm) {
-            $query->where('nombre', 'like', '%' . $searchTerm . '%');
-        })
-        ->get();
+        if (!empty($searchTerm)) {
+            $characters = Characters::whereHas('skinColor', function ($query) use ($searchTerm) {
+                $query->where('nombre', 'like', '%' . $searchTerm . '%');
+            })
+            ->orWhereHas('hairColor', function ($query) use ($searchTerm) {
+                $query->where('nombre', 'like', '%' . $searchTerm . '%');
+            })
+            ->get();
+        } else {
+            $characters = [];
+        }
 
         return view('characters_finder', compact('characters'));
     } catch (\Exception $e) {
-        // Maneja la excepción, por ejemplo, registra un mensaje de error
         return response()->json(['error' => $e->getMessage()], 500);
     }
 }
 
-
-
+/**
+ * Realiza una búsqueda de personajes con filtros de color de piel y cabello y devuelve los resultados en formato JSON.
+ *
+ * @param  \Illuminate\Http\Request  $request
+ * @return \Illuminate\Http\JsonResponse
+ */
 
 public function searchWithFilters(Request $request)
 {
     $hairColor = $request->input('hair_color');
     $skinColor = $request->input('skin_color');
 
-    // Realiza la consulta para obtener los personajes que coinciden con los filtros
-    $query = Characters::query();
+    if ($hairColor != 'Select hair color' || $skinColor != 'Select skin color') {
 
-    if ($hairColor != 'Select hair color') {
-        $query->whereHas('hairColor', function ($query) use ($hairColor) {
-            $query->where('nombre', $hairColor);
-        });
+        $query = Characters::query();
+
+        if ($hairColor != 'Select hair color') {
+            $query->whereHas('hairColor', function ($query) use ($hairColor) {
+                $query->where('nombre', $hairColor);
+            });
+        }
+
+        if ($skinColor != 'Select skin color') {
+            $query->whereHas('skinColor', function ($query) use ($skinColor) {
+                $query->where('nombre', $skinColor);
+            });
+        }
+
+        $characters = $query->get();
+    } else {
+        $characters = [];
     }
-
-    if ($skinColor != 'Select skin color') {
-        $query->whereHas('skinColor', function ($query) use ($skinColor) {
-            $query->where('nombre', $skinColor);
-        });
-    }
-
-    $characters = $query->get();
 
     return response()->json($characters);
 }
+
 }
--- resources/views/characters_finder.blade.php
+++ resources/views/characters_finder.blade.php
@@ -38,7 +38,7 @@
                 <h1 class="text-center">Human Finder</h1>
             </div>
         </div>
-        <div class="card mb-4">
+        <div class="card mb-3">
             <div class="row align-items-center mt-4">
                 <div class="col">
                     <p class="text-muted text-with-margin mb-0">Refresh the cache to load data from swapi.com</p>
@@ -47,44 +47,47 @@
 
             </div>
 
-            <div class="card-body">
+            <div class="card-body ">
                 <form method="POST" action="{{ route('search') }}">
                     @csrf
                     <div class="row mb-4">
-                        <div class="col-md-8 col-sm-12">
+                        <div class="col-md-10 col-sm-12">
                             <div class="input-group">
                                 <input class="form-control form-control-icon" id="search-term" name="search_term"
                                     aria-describedby="name" placeholder="Search by random string">
                                 <i class="fas fa-search fa-lg"></i>
                             </div>
                         </div>
-                        <div class="col-md-4 col-sm-12">
-                            <button type="submit" class="btn text-primary">SEARCH</button>
+                        <div class="col-md-1">
+                            <button type="submit" class="custom-button">SEARCH</button>
                         </div>
                     </div>
                 </form>
 
                 <div class="row">
-                    <div class="col-md-4 col-sm-6">
-                        <select id="hair-color-select" class="form-select" aria-label="Hair color selector"
-                            name="hair_color">
+                    <div class="col-md-5 col-sm-6 mb-3 ">
+                        <select id="hair-color-select" class="form-select custom-select-height"
+                            aria-label="Hair color selector" name="hair_color">
                             <option selected>Select hair color</option>
                         </select>
                     </div>
-                    <div class="col-md-4 col-sm-6">
-                        <select id="skin-color-select" class="form-select" aria-label="Skin color selector"
-                            name="skin_color">
+                    <div class="col-md-5 col-sm-6">
+                        <select id="skin-color-select" class="form-select custom-select-height"
+                            aria-label="Skin color selector" name="skin_color">
                             <option selected>Select skin color</option>
                         </select>
                     </div>
 
-                    <div class="col-md-4 col-sm-12">
-                        <button id="apply-filters-button" type="button" class="btn btn-outline-primary">APPLY
+                    <div class="col-md-2 col-sm-12 ml-n4">
+                        <button id="apply-filters-button" type="button" class="custom-button">APPLY
                             FILTERS</button>
                     </div>
                 </div>
             </div>
         </div>
+
+
+
         <div class="card mb-4" id="characters-table">
             <div class="card-body">
                 <table class="table">
@@ -120,28 +123,37 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
-    <!-- Script -->
+    <!-- Script SEARCH-->
     <script type='text/javascript'>
         $(document).ready(function () {
     $('#search-form').submit(function (e) {
-        e.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+        e.preventDefault();
 
-        var searchTerm = $('#search-term').val(); // Obtiene el término de búsqueda del usuario
+        var searchTerm = $('#search-term').val();
 
-        // Realiza una solicitud AJAX al servidor para buscar personajes
         $.ajax({
             type: 'POST',
             url: '{{ route('search') }}',
             data: { '_token': '{{ csrf_token() }}', 'search_term': searchTerm },
             success: function (data) {
                 console.log(data);
-                // Selecciona la tabla de resultados
                 var $resultsTable = $('#characters-table tbody');
+                var $noResultsMessage = $('#no-results-message');
 
-                // Borra el contenido de la tabla actual
                 $resultsTable.empty();
 
-                // Itera sobre los resultados y agrega filas a la tabla
+                if (data.length === 0) {
+
+                    $('#no-results-message').show();
+                    $('table').hide();
+                } else {
+
+                    $('#no-results-message').hide();
+                    $('table').show();
+                }
+
+
+
                 $.each(data, function (index, character) {
                     var row = '<tr><td>' + character.nombre + '</td><td>' + character.details + '</td></tr>';
                     $resultsTable.append(row);
@@ -155,9 +167,10 @@
 });
     </script>
 
+
+    <!-- Script FILL SELECT OPTIONS -->
     <script type='text/javascript'>
         $(document).ready(function () {
-    // Realiza una solicitud AJAX para obtener los colores de pelo y piel cuando se carga la página
     $.ajax({
         type: 'GET',
         url: '{{ route('getColors') }}',
@@ -165,7 +178,6 @@
             var hairColors = data.hairColors;
             var skinColors = data.skinColors;
 
-            // Llena los selects con los colores obtenidos
             var hairColorSelect = $('#hair-color-select');
             var skinColorSelect = $('#skin-color-select');
 
@@ -201,18 +213,16 @@
         }
     });
 
-    // Resto del código...
 });
     </script>
 
+    <!-- Script APPLY FILTERS -->
     <script type='text/javascript'>
         $(document).ready(function () {
-    // Maneja la búsqueda con filtros cuando se hace clic en el botón "APPLY FILTERS"
     $('#apply-filters-button').click(function () {
         var hairColor = $('#hair-color-select').val();
         var skinColor = $('#skin-color-select').val();
 
-        // Realiza una solicitud AJAX al servidor para buscar personajes con filtros
         $.ajax({
             type: 'POST',
             url: '{{ route('searchWithFilters') }}',
@@ -224,13 +234,10 @@
             },
             success: function (data) {
                 console.log(data);
-                // Selecciona la tabla de resultados
                 var $resultsTable = $('#characters-table tbody');
 
-                // Borra el contenido de la tabla actual
                 $resultsTable.empty();
 
-                // Itera sobre los resultados y agrega filas a la tabla
                 $.each(data, function (index, character) {
                     var row = '<tr><td>' + character.nombre + '</td><td>' + character.details + '</td></tr>';
                     $resultsTable.append(row);
@@ -244,6 +251,7 @@
 });
     </script>
 
+    <!-- Script POPUP -->
     <script type="text/javascript">
         $(document).ready(function () {
         // Mostrar el popup al hacer clic en el ícono
@@ -270,7 +278,7 @@
             var popup = $('<div class="character-details-popup">' + popupContent + '</div>');
             $(this).after(popup);
 
-            // Posiciona el popup junto al ícono
+            // Posiciona el popup junto al icono
             var iconPosition = $(this).position();
             popup.css({
                 top: iconPosition.top + $(this).outerHeight(),
@@ -290,12 +298,12 @@
     });
     </script>
 
-    {{-- Agrega este código en la vista para depurar --}}
-    @if(isset($characters))
+    {{--  para depurar --}}
+    {{--@if(isset($characters))
     @php
     dd($characters);
     @endphp
-    @endif
+    @endif--}}
 
 
     </div>
